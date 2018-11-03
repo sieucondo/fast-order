d5b67d169d0c2e6802972f4841922a48195cd8dd
Gian Tran <gianqng97@gmail.com>
2018-11-04 03:03:51 +0700

    Add edit quantity, load img from URL, change icon button

--- app/src/main/java/com/android/fastorder/CartActivity.java
+++ app/src/main/java/com/android/fastorder/CartActivity.java
@@ -1,5 +1,6 @@
 package com.android.fastorder;
 
+import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.ActionBar;
@@ -7,16 +8,23 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 import java.text.DecimalFormat;
 import java.text.NumberFormat;
 import java.util.ArrayList;
 import java.util.List;
+
 import adapter.CartAdapter;
 import model.Cart;
 import model.CartItem;
@@ -60,21 +68,35 @@ public class CartActivity extends AppCompatActivity {
 
 
     public void clickToOrder(View view) {
-        if(ProductCart.getCart().getListProduct().size() > 0){
+        if (ProductCart.getCart().getListProduct().size() > 0) {
+            createJsonOrder();
             CreateBill();
             updateOrderInfo();
             Toast.makeText(CartActivity.this, "Thanks for you order :)", Toast.LENGTH_SHORT).show();
             finish();
-        }else {
+        } else {
             Toast.makeText(CartActivity.this, "Add something to your cart!", Toast.LENGTH_SHORT).show();
         }
     }
 
-    public void CreateBill(){
+    public String createJsonOrder() {
+        Gson gsonBuilder = new GsonBuilder().create();
+        String json;
+        Cart cart = ProductCart.getCart();
+        List<Product> listProduct = new ArrayList<>();
+//        for(CartItem p: cart.getListProduct()){
+//            listProduct.add(p.getProduct());
+//        }
+        json = gsonBuilder.toJson(cart.getListProduct());
+        Log.e("Json", json);
+        return json;
+    }
+
+    public void CreateBill() {
         Cart bill = ProductPayment.getBill();
         Cart cart = ProductCart.getCart();
         List<CartItem> listItem = bill.getListProduct();
-        for(CartItem c: cart.getListProduct()){
+        for (CartItem c : cart.getListProduct()) {
             if (listItem.contains(c)) {
                 int index = listItem.indexOf(c);
                 int quantity = listItem.get(index).getQuantity();
@@ -98,6 +120,70 @@ public class CartActivity extends AppCompatActivity {
         showDialogRemove(position);
     }
 
+    public void clickToEdit(View view) {
+        View parentRow = (View) view.getParent();//Nhan view cha cua btn
+        ListView listView = (ListView) parentRow.getParent();//Nhan view cha cua itemView
+        final int position = listView.getPositionForView(parentRow);//Lay vi tri itemView tu view cha
+
+        final Dialog dialog = new Dialog(this);
+        dialog.setContentView(R.layout.edit_dialog);
+
+        final TextView txtQuantity = dialog.findViewById(R.id.txtEditQuantity);
+        int quantity = productCartAdapter.getListCart().get(position).getQuantity();
+        txtQuantity.setText(String.valueOf(quantity));
+
+
+        Button btnIncrease = dialog.findViewById(R.id.btnIncrease);
+        btnIncrease.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int quantity = Integer.parseInt(txtQuantity.getText().toString());
+                quantity += 1;
+                txtQuantity.setText(String.valueOf(quantity));
+            }
+        });
+
+        Button btnDecrease = dialog.findViewById(R.id.btnDecrease);
+        btnDecrease.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int quantity = Integer.parseInt(txtQuantity.getText().toString());
+                quantity -= 1;
+                if (quantity < 1) {
+                    quantity = 1;
+                }
+                txtQuantity.setText(String.valueOf(quantity));
+            }
+        });
+
+        Button btnConfirm = dialog.findViewById(R.id.btnConfirm);
+        btnConfirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                productCartAdapter.getListCart().get(position).setQuantity(Integer.parseInt(txtQuantity.getText().toString()));
+                productCartAdapter.notifyDataSetChanged();
+
+                Cart cart = ProductCart.getCart();
+                cart.setListProduct(productCartAdapter.getListCart());
+                cart.setCartInfo();
+                updateOrderInfo();
+                dialog.dismiss();
+            }
+        });
+
+        Button btnCancel = dialog.findViewById(R.id.btnCancel);
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+        dialog.setTitle("Edit Product Quantity");
+        dialog.setCancelable(false);
+        dialog.show();
+
+    }
 
 
     private List<CartItem> getListData() {
@@ -124,7 +210,7 @@ public class CartActivity extends AppCompatActivity {
         txtTotalPrice.setText(formatter.format(cart.getTotalPrice()) + " đ");
     }
 
-    public void showDialogRemove(final int position){
+    public void showDialogRemove(final int position) {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle("Remove Product");
         builder.setMessage(R.string.confirm_remove_product);
